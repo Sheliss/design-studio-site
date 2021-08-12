2ebaa561643963070573884c3fc0ce64fe97980e
Sheliss <sheliss.kai@gmail.com>
2021-08-12 13:43:02 +0300

    added routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.scss';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Footer from './Components/Footer';
+import About from './Components/About';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Router>
+        <Header/>
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About}/>
+        </Switch>
+        <Footer/>
+      </Router>
     </div>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Nav, Navbar, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import SiteLogo from '../assets/logo.png'
 
 export default function Header() {
@@ -12,9 +13,9 @@ export default function Header() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
                 <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-lg-end">
                     <Nav className="nav__custom">
-                        <Nav.Link href="/" className="nav__btn">Home</Nav.Link>
-                        <Nav.Link href="/" className="nav__btn">About</Nav.Link>
-                        <Nav.Link href="/" className="nav__btn">Contact</Nav.Link>
+                        <Nav.Link as={Link} to="/" className="nav__btn">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/about" className="nav__btn">About</Nav.Link>
+                        <Nav.Link as={Link} href="/" className="nav__btn">Contact</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
